a82c9c53ab5ac48c389b32abb985f5e60905e7a5
Jazza231 <greeny-231@outlook.com>
2025-01-02 16:11:18 +0800

    New auth test

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -1,23 +1,28 @@
-import { redirect, type Actions } from "@sveltejs/kit";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
-import { invalidateSession } from "$lib/server/auth";
 import * as auth from "$lib/server/auth";
 import { db } from "$lib/server/db";
 import { mockImage, mockSuggestion } from "$lib/mockData";
 import { table } from "$lib/server";
 
-export const load = (async ({ locals: { session, user } }) => {
-	if (!session) redirect(302, "/login");
+export const load = (async ({ locals: { user } }) => {
+	if (!user) redirect(302, "/login");
 
 	return { user };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
-	logout: async ({ locals: { session }, cookies }) => {
-		if (session) {
-			invalidateSession(session.id);
-			cookies.delete(auth.sessionCookieName, { path: "/" });
+	logout: async (event) => {
+		const {
+			locals: { session },
+		} = event;
+
+		if (!session) {
+			return fail(401);
 		}
+		await auth.invalidateSession(session.id);
+		auth.deleteSessionTokenCookie(event);
+
 		redirect(302, "/");
 	},
 	suggestion: async ({ locals: { session } }) => {
