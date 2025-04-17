55b7c33051750dafb379e38317c8d809c822d116
Jazza231 <greeny-231@outlook.com>
2025-04-17 10:35:06 +0800

    Authorisation

--- src/routes/api/keepalive/+server.ts
+++ src/routes/api/keepalive/+server.ts
@@ -1,7 +1,11 @@
 import { redis } from "$lib/server/redis";
 import type { RequestHandler } from "@sveltejs/kit";
 
-export const GET: RequestHandler = async () => {
+export const GET: RequestHandler = async ({ request }) => {
+	if (request.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`) {
+		return new Response("Unauthorized", { status: 401 });
+	}
+
 	await redis.set("keepalive", Date.now(), "EX", 60);
-	return new Response(null, { status: 200 });
+	return new Response("ok", { status: 200 });
 };
